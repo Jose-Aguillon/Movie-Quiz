569b70d558242e5e27f952f7e621ade360701112
Jose-Aguillon <99156545+Jose-Aguillon@users.noreply.github.com>
2022-10-18 21:22:49 -0700

    simplified button func, fixed constraints on homepage, added a next button, and cleaned up the didViewLoad func

--- MovieQuizz/MovieQuizz/ViewController.swift
+++ MovieQuizz/MovieQuizz/ViewController.swift
@@ -25,11 +25,19 @@ class Movie {
   }
 }
 
+
+/*
+ DATABASE OF MOVIES
+ */
 var movie1 = Movie(answer: "Morbius", genre: "Action, Adventure, Horror", actors: "Jared Leto, Matt Smith", summary: "Biochemist Michael Morbius tries to cure himself of a rare blood disease, but he inadvertently infects himself with a form of vampirism instead.", number:1)
 var movie2 = Movie(answer: "Fight Club", genre: "Drama, Action", actors: "Brad Pitt, Edward Norton", summary: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.", number:1)
 var movie3 = Movie(answer: "Bee Movie", genre: "Adventure, Comedy", actors: "Jerry Seinfeld, Renee Zellweger", summary: "A bee that just graduated from college is dillusioned by his lone career choice: making honey.", number:1)
 var movie4 = Movie(answer: "The Avengers", genre: "Action, Adventure", actors: "Robert Downey Jr, Chris Evans", summary: "Earths mightiest heroes must come together and learn to fight as a team to stop an invading alien army.", number:1)
 
+
+/*
+ LIST OF MOVIES
+ */
 var movies = [movie1, movie2, movie3, movie4]
 
 class SecondView: UIViewController {
@@ -45,8 +53,11 @@ class ViewController: UIViewController {
     @IBOutlet var buttonAns2: UIButton!
     @IBOutlet var buttonAns3: UIButton!
     @IBOutlet var buttonAns4: UIButton!
+    @IBOutlet var nextQuestion: UIButton!
     
-    
+    /*
+     Output for Each Question
+     */
     let numQuestions: [String] = [
     "YOU LOSE",
     "Question 1",
@@ -56,9 +67,13 @@ class ViewController: UIViewController {
     ]
     
         
-    
+    /*
+     Generates the First Question
+     */
     var randQuestion = movies[Int.random(in: 0..<3)]
-    
+    /*
+     List of Hints
+     */
     var question1 : String {
         get {
             return "The genre is: \(randQuestion.genre)"
@@ -79,192 +94,133 @@ class ViewController: UIViewController {
             return " "
         }
     }
+   
     
-    let randNum = Int.random(in:1..<4)
-    
+    /*
+     Determines what question to input
+     Random number 1-3
+     */
     let questions = [1, 2, 3, 4]
+    var randNum = Int.random(in:1..<4)
     
-    var storedans = [movie1]
-//    var questions: [String] = [
-//        " ",
-//        "The genre is: \(movie1.genre)",
-//        "The two main actors are: \(movie1.actors)",
-//        "The summary for the movie is: \(movie1.summary)"
-//    ]
+
+    
     var currentQuestionIndex: Int = 1
     var currentNumberIndex: Int = 1
     
-    @IBAction func button1(_ sender: UIButton) {
-        if buttonAns1.currentTitle != randQuestion.answer {
+    /*
+     Function For All Buttona --------------------
+     */
+    @IBAction func button(_ sender: UIButton) {
+        if sender.currentTitle != randQuestion.answer {
             currentQuestionIndex += 1
             currentNumberIndex += 1
+            sender.isHidden = true
+        } else {
+            currentNumberIndex = 4
+            currentQuestionIndex = 0
+        }
+        if currentQuestionIndex == questions.count || currentNumberIndex == numQuestions.count{
+            currentQuestionIndex = 0
+            currentNumberIndex = 0
+        }
+        if currentQuestionIndex == 1 {
+            let question: String = question1
+            questionLabel.text = question
+        } else if currentQuestionIndex == 2{
+            let question: String = question2
+            questionLabel.text = question
+        } else if currentQuestionIndex == 3{
+            let question: String = question3
+            questionLabel.text = question
+        } else {
+            let question: String = question4
+            questionLabel.text = question
+        }
+        let numQuestion: String = numQuestions[currentNumberIndex]
+        questionNumLabel.text = numQuestion
+        if questionNumLabel.text == numQuestions[0] || questionNumLabel.text == numQuestions[4]{
+            nextQuestion.isHidden = false
             buttonAns1.isHidden = true
-        } else {
-            currentNumberIndex = 4
-            currentQuestionIndex = 0
-        }
-        if currentQuestionIndex == questions.count || currentNumberIndex == numQuestions.count{
-            currentQuestionIndex = 0
-            currentNumberIndex = 0
-        }
-        if currentQuestionIndex == 1 {
-            let question: String = question1
-            questionLabel.text = question
-        } else if currentQuestionIndex == 2{
-            let question: String = question2
-            questionLabel.text = question
-        } else if currentQuestionIndex == 3{
-            let question: String = question3
-            questionLabel.text = question
-        } else {
-            let question: String = question4
-            questionLabel.text = question
-        }
-        let numQuestion: String = numQuestions[currentNumberIndex]
-        questionNumLabel.text = numQuestion
-    }
-    
-    @IBAction func button2(_ sender: UIButton) {
-        if buttonAns2.currentTitle != randQuestion.answer {
-            currentQuestionIndex += 1
-            currentNumberIndex += 1
             buttonAns2.isHidden = true
-        } else {
-            currentNumberIndex = 4
-            currentQuestionIndex = 0
-        }
-        if currentQuestionIndex == questions.count || currentNumberIndex == numQuestions.count{
-            currentQuestionIndex = 0
-            currentNumberIndex = 0
-        }
-        if currentQuestionIndex == 1 {
-            let question: String = question1
-            questionLabel.text = question
-        } else if currentQuestionIndex == 2{
-            let question: String = question2
-            questionLabel.text = question
-        } else if currentQuestionIndex == 3{
-            let question: String = question3
-            questionLabel.text = question
-        } else {
-            let question: String = question4
-            questionLabel.text = question
-        }
-        let numQuestion: String = numQuestions[currentNumberIndex]
-        questionNumLabel.text = numQuestion
-    }
-    
-    @IBAction func button3(_ sender: UIButton) {
-        if buttonAns3.currentTitle != randQuestion.answer {
-            currentQuestionIndex += 1
-            currentNumberIndex += 1
             buttonAns3.isHidden = true
-        } else {
-            currentNumberIndex = 4
-            currentQuestionIndex = 0
-        }
-        if currentQuestionIndex == questions.count || currentNumberIndex == numQuestions.count{
-            currentQuestionIndex = 0
-            currentNumberIndex = 0
-        }
-        if currentQuestionIndex == 1 {
-            let question: String = question1
-            questionLabel.text = question
-        } else if currentQuestionIndex == 2{
-            let question: String = question2
-            questionLabel.text = question
-        } else if currentQuestionIndex == 3{
-            let question: String = question3
-            questionLabel.text = question
-        } else {
-            let question: String = question4
-            questionLabel.text = question
-        }
-        let numQuestion: String = numQuestions[currentNumberIndex]
-        questionNumLabel.text = numQuestion
-    }
-    
-    @IBAction func button4(_ sender: UIButton) {
-        if buttonAns4.currentTitle != randQuestion.answer {
-            currentQuestionIndex += 1
-            currentNumberIndex += 1
             buttonAns4.isHidden = true
-        } else {
-            currentNumberIndex = 4
-            currentQuestionIndex = 0
         }
-        if currentQuestionIndex == questions.count || currentNumberIndex == numQuestions.count{
-            currentQuestionIndex = 0
-            currentNumberIndex = 0
-        }
-        if currentQuestionIndex == 1 {
-            let question: String = question1
-            questionLabel.text = question
-        } else if currentQuestionIndex == 2{
-            let question: String = question2
-            questionLabel.text = question
-        } else if currentQuestionIndex == 3{
-            let question: String = question3
-            questionLabel.text = question
-        } else {
-            let question: String = question4
-            questionLabel.text = question
-        }
-        let numQuestion: String = numQuestions[currentNumberIndex]
-        questionNumLabel.text = numQuestion
     }
+    /*
+     Shows the Next Question Button and Creates a New Question --------------------
+     */
+    @IBAction func nextQuestButton(_ sender: UIButton) {
+        if questionNumLabel.text == numQuestions[0] || questionNumLabel.text == numQuestions[4]{
+            for movie in movies{
+                movie.number = 1
+            }
+            randQuestion = movies[Int.random(in: 0..<3)]
+            randNum = Int.random(in:1..<4)
+            questionNumLabel.text = numQuestions[1]
+            questionLabel.text = question1
+            currentQuestionIndex = 1
+            currentNumberIndex = 1
+            generateTitle()
+            buttonAns1.isHidden = false
+            buttonAns2.isHidden = false
+            buttonAns3.isHidden = false
+            buttonAns4.isHidden = false
+        }
+        sender.isHidden = true
+    }
+    /*
+     Check for duplicates, send string to text boxes --------------------
+     */
     func generate() -> String{
         var newMovie = movies[Int.random(in: 0..<4)]
         while (newMovie.number == 0 ) {
             newMovie = movies[Int.random(in: 0..<4)]
-            print(newMovie.answer)
         }
         newMovie.number = 0
         return newMovie.answer
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    func generateTitle() {
         randQuestion.number = 0
+        
+        //Placing a Title in Button 1
         if randNum == 1{
             buttonAns1.setTitle(randQuestion.answer, for: UIControl.State.normal)
         } else {
             buttonAns1.setTitle(generate(), for: UIControl.State.normal)
+            
+        //Placing a Title in Button 2
         }
         if randNum == 2{
             buttonAns2.setTitle(randQuestion.answer, for: UIControl.State.normal)
         } else {
             buttonAns2.setTitle(generate(), for: UIControl.State.normal)
         }
+        
+        //Placing a Title in Button 3
         if randNum == 3{
             buttonAns3.setTitle(randQuestion.answer, for: UIControl.State.normal)
         } else {
             buttonAns3.setTitle(generate(), for: UIControl.State.normal)
         }
+        
+        //Placing a Title in Button 4
         if randNum == 4{
             buttonAns4.setTitle(randQuestion.answer, for: UIControl.State.normal)
         } else {
             buttonAns4.setTitle(generate(), for: UIControl.State.normal)
         }
-        if currentQuestionIndex == 1 {
-            let question: String = question1
-            questionLabel.text = question
-        } else if currentQuestionIndex == 2{
-            let question: String = question2
-            questionLabel.text = question
-        } else if currentQuestionIndex == 3{
-            let question: String = question3
-            questionLabel.text = question
-        } else {
-            let question: String = question4
-            questionLabel.text = question
-        }
-        questionNumLabel.text = numQuestions[currentNumberIndex]
-        
     }
-
+    
+    //Generating Random Questions, and Different Spots
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        nextQuestion.isHidden = true
+        questionLabel.text = question1
+        generateTitle()
+    }
 
 }
 
